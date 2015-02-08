3962c2595f8ed4fc6a2611d73e077e65423e53a0
piotrmirowski <piotr.mirowski@computer.org>
2015-02-08 20:46:37 +0000

    Try to remove weird template syntax

--- DependencyTreeRNN++/CorpusWordReader.h
+++ DependencyTreeRNN++/CorpusWordReader.h
@@ -10,51 +10,42 @@
 #include <algorithm>
 #include <cctype>
 
-/// <summary>
-/// These functions are part of an internal library that should be included
-/// </summary>
-template <typename T>
-inline std::string pop_first_word(std::string& s, T ISSPACE)
-{
-    const auto p1 = std::find_if(s.begin(), s.end(), [ISSPACE](char c) { return !ISSPACE(c); });
-    const auto p2 = std::find_if(p1, s.end(), ISSPACE);
-    const std::string word(p1, p2);
-    s.erase(0, std::find_if(p2, s.end(), [ISSPACE](char c) { return !ISSPACE(c); }) - s.begin());
-    return word;
-}
-inline std::string pop_first_word(std::string& s)
-{
-    return pop_first_word(s, [](char c) { return isspace(c); });
-}
+
+inline bool isSpace(char c) { return isspace(c); };
+inline bool notIsSpace(char c) { return isspace(c); };
 
 
 /// <summary>
 /// Simple class to read words, one by one, from a file.
 /// When the end of a line is reached, it returns "</s>"
 /// </summary>
-class WordReader
-{
+class WordReader {
+protected:
     std::ifstream m_file;
     std::string m_line;
     
 public:
     
     WordReader(const std::string &filename)
-    : m_file(filename)
-    {
+    : m_file(filename) {
     }
     
-    std::string get_next()
-    {
+    
+    std::string pop_first_word(std::string& s) {
+        const auto p1 = std::find_if(s.begin(), s.end(), notIsSpace);
+        const auto p2 = std::find_if(p1, s.end(), isSpace);
+        const std::string word(p1, p2);
+        s.erase(0, std::find_if(p2, s.end(), notIsSpace) - s.begin());
+        return word;
+    }
+    
+    
+    std::string get_next() {
         std::string result;
-        if (m_line.empty())
-        {
-            if (std::getline(m_file, m_line))
-            {
+        if (m_line.empty()) {
+            if (std::getline(m_file, m_line)) {
                 m_line += " </s>";
-            }
-            else
-            {
+            } else {
                 return result;
             }
         }
