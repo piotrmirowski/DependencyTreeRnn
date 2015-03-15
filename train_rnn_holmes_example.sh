ed5b3872eaba991d3021656814080dd4ee3bde0e
piotrmirowski <piotr.mirowski@computer.org>
2015-03-15 22:49:09 +0000

    Use of vocabulary files

--- train_rnn_holmes_example.sh
+++ train_rnn_holmes_example.sh
@@ -5,8 +5,10 @@
 PATH_JSON="/Users/piotr/Documents/Projets/Microsoft/Data/GutenbergHolmes/"
 PATH_JSON_PIOTR_MAC="/Users/piotr/Documents/Projets/Microsoft/Data/GutenbergHolmes/"
 
-DEP_LABELS=2
+# Define the minimum number of word occurrences as 5 and use existing vocabulary file
 MIN_WORD_OCCURRENCE=5
+
+DEP_LABELS=2
 RNN_HIDDENS=100
 RNN_CLASSES=500
 NGRAM_SIZE_MB=2000
@@ -21,6 +23,7 @@ PATH_MODELS="./models"
 LIST_VALID=$PATH_DATA"/valid.txt"
 LIST_TRAIN=$PATH_DATA"/train.txt"
 FILE_SENTENCE_LABELS=$PATH_DATA"/valid.labels"
+FILE_VOCAB=$PATH_DATA"/vocab_mw"$MIN_WORD_OCCURRENCE".txt"
 FILE_MODEL=$PATH_MODELS"/GutenbergHolmes_p"$DEP_LABELS
 FILE_MODEL=$FILE_MODEL"_mw"$MIN_WORD_OCCURRENCE
 FILE_MODEL=$FILE_MODEL"_h"$RNN_HIDDENS
@@ -35,6 +38,7 @@ echo "RNN model will be stored in $FILE_MODEL..."
 # Train the dependency-parsing model
 RnnDependencyTree \
   -rnnlm $FILE_MODEL \
+  -vocab $FILE_VOCAB \
   -train $LIST_TRAIN \
   -valid $LIST_VALID \
   -sentence-labels $FILE_SENTENCE_LABELS \
@@ -45,6 +49,7 @@ RnnDependencyTree \
   -direct $NGRAM_SIZE_MB \
   -direct-order $NGRAM_ORDER \
   -bptt $BPTT_ORDER \
+  -bptt-block 1 \
   -class $RNN_CLASSES \
   -feature-gamma $FEATURE_GAMMA \
   -debug false
